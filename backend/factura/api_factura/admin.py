471e32402c8cf32d5e85d2e2b45bb96fc6d41d75
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-17 09:00:14 -0600

    refactor: 💡 colocando las vistas en el admin de django

--- backend/factura/api_factura/admin.py
+++ backend/factura/api_factura/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from .models import Cliente
+from .models import Cliente, Categoria , Factura,Descuentos, Factura_descuentos,Factura_detalle, Impuestos, Producto
 
 # Register your models here.
 admin.site.register(Cliente)
+admin.site.register(Factura)
+admin.site.register(Producto)
+admin.site.register(Descuentos)
+admin.site.register(Factura_descuentos)
+admin.site.register(Factura_detalle)
+admin.site.register(Categoria)
+admin.site.register(Impuestos)
