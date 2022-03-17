ef18da217f20e691bbc18361af690db852ce01b4
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-16 23:58:36 -0600

    feat: 🎸 primer modelo y llamarlo al admin

--- backend/factura/api_factura/models.py
+++ backend/factura/api_factura/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    identidad = models.CharField(max_length=13)
+    nombre_completo = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=45)
+    telefono = models.CharField(max_length=8)
